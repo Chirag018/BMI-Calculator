999df83df024028a3fbe49559c892f805316cd16
Chirag018 <chiragmmehta16@gmail.com>
2020-08-16 16:05:31 +0530

    few changes

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
- const Color widgetsColor=Color(0xFF1D1E33);
- const double bottomHeight=80.0;
- const bottomContainerColor=Color(0xFFEB1555);
+const Color widgetsColor = Color(0xFF1D1E33);
+const double bottomHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -24,11 +25,19 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                 child: ReusableCard(
                   colour: widgetsColor,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'MALE',
+                  ),
                 ),
               ),
               Expanded(
                 child: ReusableCard(
                   colour: widgetsColor,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.venus,
+                    label: 'FEMALE',
+                  ),
                 ),
               ),
             ],
@@ -66,15 +75,46 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class ReusableCard extends StatelessWidget {
-  Color colour;
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String label;
 
-  ReusableCard({@required this.colour});
+  IconContent({this.icon, this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+  final Widget cardChild;
+
+  ReusableCard({@required this.colour, this.cardChild});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardChild,
       decoration: BoxDecoration(
         color: colour,
         borderRadius: BorderRadius.circular(10.0),
